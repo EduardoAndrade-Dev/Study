2ca92f9e43d041caf12289b061be9d840591402d
eduardo andrade <eduardo.andrade@logitravelgroup.com>
2020-08-03 20:12:32 +0200

    Add controller, add connection string an migration of DB.

--- CrudEntityFramework/CrudEntityFramework/Controllers/HomeController.cs
+++ CrudEntityFramework/CrudEntityFramework/Controllers/HomeController.cs
@@ -1,26 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
+﻿using CrudEntityFramework.Data;
 using CrudEntityFramework.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+using System.Threading.Tasks;
 
 namespace CrudEntityFramework.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly ApplicationDbContext _context;
+        public HomeController(ApplicationDbContext context)
         {
-            _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        [HttpGet]
+        public async Task<IActionResult> Index()
         {
-            return View();
+            return View(await _context.Usuario.ToListAsync());
         }
 
         public IActionResult Privacy()
