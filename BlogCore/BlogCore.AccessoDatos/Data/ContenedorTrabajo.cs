05f1c7e7d3377c761e732790d69538ed03c53bc0
eduardo andrade <eduardo.andrade@logitravelgroup.com>
2020-08-10 18:41:03 +0200

    Add ConnectionString, add-migration and createDatabase(update-database)

---
+++ BlogCore/BlogCore.AccessoDatos/Data/ContenedorTrabajo.cs
@@ -0,0 +1,27 @@
+﻿using BlogCore.AccessoDatos.Data.Repository;
+
+namespace BlogCore.AccessoDatos.Data
+{
+    public class ContenedorTrabajo : IContenedorTrabajo
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ContenedorTrabajo(ApplicationDbContext db)
+        {
+            _db = db;
+            Categoria = new CategoriaRepository(_db);
+        }
+
+        public ICategoriaRepository Categoria { get; private set; }
+
+        public void Dispose()
+        {
+            _db.Dispose();
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
---
+++ BlogCore/BlogCore.AccessoDatos/Data/Repository/IContenedorTrabajo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace BlogCore.AccessoDatos.Data.Repository
+{
+    public interface IContenedorTrabajo : IDisposable
+    {
+        ICategoriaRepository Categoria { get; }
+
+        void Save();
+    }
+}
