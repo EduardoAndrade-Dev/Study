d9705b98a83494809f9e5b21dfd8f7be98bfaa4c
EduardoAndrade-Dev <eduardo.padilhaandrade@gmail.com>
2020-09-13 18:50:51 +0200

    reutilizando tarjetas

--- Angular/02-SPA/src/app/app.module.ts
+++ Angular/02-SPA/src/app/app.module.ts
@@ -17,6 +17,7 @@ import { AboutComponent } from './components/about/about.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
 import { HeroeComponent } from './components/heroe/heroe.component';
 import { HeroesResultComponent } from './components/heroes-result/heroes-result.component';
+import { HeroeTarjetaComponent } from './components/heroe-tarjeta/heroe-tarjeta.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import { HeroesResultComponent } from './components/heroes-result/heroes-result.
     AboutComponent,
     HeroesComponent,
     HeroeComponent,
-    HeroesResultComponent
+    HeroesResultComponent,
+    HeroeTarjetaComponent
   ],
   imports: [
     BrowserModule,
